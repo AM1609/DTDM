7aee363cbd4e786b461f1f3021d8337da7569b20
AM1609 <anhminh16092002@gmail.com>
2023-10-20 09:11:06 +0700

    up

--- src/navigation/CustomNavigationBar_Lab2.js
+++ src/navigation/CustomNavigationBar_Lab2.js
@@ -7,6 +7,8 @@ import ProfileScreen from "../screens/ProfileScreen";
 import colors from '../utility/colors';
 import { createMaterialBottomTabNavigator } from "@react-navigation/material-bottom-tabs";
 import User from "../screens/User";
+import Option from "../screens/Option";
+import { MaterialIcons } from '@expo/vector-icons';
 // import ProfileScreen from "../src/screens/ProfileScreen";
 // import ContactsScreen from "..src/screens/ContactsScreen";
 const Stack = createStackNavigator();
@@ -32,43 +34,47 @@ const ProScreen = () =>{
   )
 
 }
+const Pro1Screen = ({navigation}) =>{
+  return(
+    
+    <Stack.Navigator
+      initialRouteName="User"
+      screenOptions={
+        {
+          // header: (props)=> <AppBarHeaderMenu {...props} />,
+          headerTintColor: "white",
+          headerStyle: {backgroundColor:colors.blue},
+          headerTitleAlign: "center",  
+        }
+      }
+    >
+      <Stack.Screen name="User" component={User} options={{title:"User",
+      headerTitle:"Me", 
+      headerTintColor: 'white', 
+      headerStyle: { backgroundColor: colors.blue, }, 
+      headerRight: ()=>(
+        <MaterialIcons name="settings" 
+          size={24} 
+          style={{ color: 'white', marginRight: 10 }} onPress={() => navigation.navigate("Option")} />
+      )
+      }}
+      />
+      
+    
+      <Stack.Screen name="Option" component={Option} />
+    </Stack.Navigator>
+    
+  )
+
+}
 const CustomNavigationBar_Lab2 = () => {
   return (
     <NavigationContainer>
-      {/* <Stack.Navigator
-        initialRouteName="Contacts"
-        screenOptions={
-          {
-            // header: (props)=> <AppBarHeaderMenu {...props} />,
-            headerTintColor: "white",
-            headerStyle: {backgroundColor:colors.blue},
-            headerTitleAlign: "center",  
-          }
-        }
-      >
-        <Stack.Screen name="Contacts" component={ContactsScreen} options={{title:"Contacts"}}/>
-        <Stack.Screen name="ProfileScreen" component={ProfileScreen} />
-      </Stack.Navigator> */}
       <Tab.Navigator>
         <Tab.Screen name="Contacts"  component={ProScreen} options={{tabBarIcon: "home"}}/>
-        <Tab.Screen name="User"  component={User} options={{tabBarIcon: "home"}}/>
+        <Tab.Screen name="User"  component={Pro1Screen} options={{tabBarIcon: "home"}}/>
       </Tab.Navigator>
     </NavigationContainer>
-    // <NavigationContainer>
-    //   <Tab.Navigator
-    //   initialRouteName="Contacts"
-    //   screenOptions={
-    //      {
-    //       // header: (props)=> <AppBarHeaderMenu {...props} />,
-    //       headerTintColor: "white",
-    //       headerStyle: {backgroundColor:colors.blue},
-    //       headerTitleAlign: "center",  
-    //     }
-    //   }>
-    //   <Tab.Screen name="Contacts" component={ContactsScreen} options={{title:"Contacts"}}/>
-    //     <Tab.Screen name="ProfileScreen" component={ProfileScreen} />
-    //   </Tab.Navigator>
-    // </NavigationContainer>
   );
 }
 
